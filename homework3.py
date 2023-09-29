674ff3bf21711b50a9fef97299d48e843c7d965a
asala ali <76118859+asalaali@users.noreply.github.com>
2023-09-29 22:34:43 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#asala ali 
+#445306890
+
+total = 44 + 33 + \
+ 10 
+
+print(total)
+
+#asala ali 
+#445306890
+
+
+# In[2]:
+
+
+x = 5
+y = 6
+
+print('Area of rectangle', x * y)
+
+
+# In[3]:
+
+
+num1 = 20
+num2 = 20
+if num1 > num2:
+    print(num1, 'is greater than', num2)
+elif num2 > num1:
+    print(num2, 'is greater than', num1)
+else:
+    print('number1 and number2 are equal')
+
+
+# In[4]:
+
+
+num1 = 30
+if num1 > 10:
+    if num1 % 2 == 0:
+        print('Even number is greater than 10')
+
+
+# In[5]:
+
+
+num1 = 3
+if num1 > 10:
+    if num1 % 2 == 0:
+        print('Even number is greater than 10')
+else:
+    print('the number is smaller than 10')
+    
+#asala ali
+#445306890
+
+
+# In[ ]:
+
+
+
+
